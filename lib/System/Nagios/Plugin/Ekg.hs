a9df084ece2f399f204db35f8807c913ca271f12
Sharif Olorin <sio@tesser.org>
2015-05-31 04:06:46 +0000

    Initial cabal things

---
+++ lib/System/Nagios/Plugin/Ekg.hs
@@ -0,0 +1,9 @@
+module System.Nagios.Plugin.Ekg where
+
+import System.Nagios.Plugin
+import Data.Aeson
+import Data.Text (Text)
+
+checkEkg :: Text
+         -> NagiosPlugin ()
+checkEkg uri = undefined
