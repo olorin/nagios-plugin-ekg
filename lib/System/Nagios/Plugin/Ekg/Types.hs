9a797a3fe688dc9d0880bcded0b8907e328df6ec
Sharif Olorin <sio@tesser.org>
2015-06-24 07:08:24 +0000

    FromJSON instances for EKG things

--- lib/System/Nagios/Plugin/Ekg/Types.hs
+++ lib/System/Nagios/Plugin/Ekg/Types.hs
@@ -2,8 +2,13 @@
 
 module System.Nagios.Plugin.Ekg.Types where
 
+import Control.Applicative
+import Control.Monad
 import Data.Aeson
+import Data.Aeson.Types
 import Data.Int
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HM
 import Data.Monoid
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -29,3 +34,25 @@ instance FromJSON EkgMetric where
             "d" -> return EkgDistribution
             x   -> fail $ "Invalid metric type " <> T.unpack x
     parseJSON _          = fail "EkgMetric must be an object"
+
+data MetricNode =
+      Leaf EkgMetric
+    | Branch (HashMap Text MetricNode)
+
+instance FromJSON MetricNode where
+    parseJSON (Object o) = do
+        leaf <- isLeaf <$> parseJSON (Object o)
+        if leaf
+            then Leaf <$> parseJSON (Object o)
+            else Branch <$> parseJSON (Object o)
+      where
+        isLeaf :: HashMap Text Value -> Bool
+        isLeaf m = HM.member "type" m && HM.member "val" m
+    parseJSON _          = fail "MetricNode must be an object"
+
+newtype MetricTree = MetricTree
+    { unMetricTree :: HashMap Text MetricNode }
+
+instance FromJSON MetricTree where
+    parseJSON (Object o) = MetricTree <$> parseJSON (Object o)
+    parseJSON _          = fail "MetricTree must be an object"
